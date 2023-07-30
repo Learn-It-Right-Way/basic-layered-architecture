b5c2bfb4e8ada108fb6997193be5db0202da9993
Dilan Madhusanka <kadm0128@gmail.com>
2023-07-30 13:56:13 +0800

    CHG | Code changes for issues fixed

--- server/src/index.js
+++ server/src/index.js
@@ -1,40 +1,40 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
+// const fs = require('fs');
+// const path = require('path');
+// const cors = require('cors');
 
 const hostname = 'layered-poc-pure';
 const port = 3200;
 
 const server = http.createServer((req, res) => {
    // Set up CORS headers
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  //  res.setHeader('Access-Control-Allow-Origin', '*');
+  //  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
  
-   // Handle preflight OPTIONS request
-   if (req.method === 'OPTIONS') {
-     res.writeHead(204);
-     res.end();
-     return;
-   }
+  //  // Handle preflight OPTIONS request
+  //  if (req.method === 'OPTIONS') {
+  //    res.writeHead(204);
+  //    res.end();
+  //    return;
+  //  }
  
-   // Handle other requests
-  if (req.method === 'GET' && req.url === '/api/books') {
-    fs.readFile('json-database/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.log(err);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+  //  // Handle other requests
+  // if (req.method === 'GET' && req.url === '/api/books') {
+  //   fs.readFile('json-database/db.json', 'utf8', (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //       res.writeHead(500, { 'Content-Type': 'text/plain' });
+  //       res.end('Internal Server Error');
+  //       return;
+  //     }
+  //     res.writeHead(200, { 'Content-Type': 'application/json' });
+  //     res.end(data);
+  //   });
+  // } else {
+  //   res.writeHead(404, { 'Content-Type': 'text/plain' });
+  //   res.end('Not Found');
+  // }
 });
 
 server.listen(port, hostname, () => {
